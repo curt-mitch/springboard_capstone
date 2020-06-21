1dfbfbe92c759103ab34049b17eac55cab0a6e51
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-06-20 18:10:59 -0700

    Creating dicts of paragraphs and sentence info

--- data_parsing/xml_parser.py
+++ data_parsing/xml_parser.py
@@ -1,5 +1,6 @@
 import os
 import xml.etree.ElementTree as ET
+import json
 
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, './../datasets/japanese_english_bilingual_corpus_wikipedia_kyoto/BDS00389.xml')
@@ -7,14 +8,32 @@ tree = ET.parse(filename)
 root = tree.getroot()
 
 # get title values
-title_el = root.find('tit')
+title_els = root.findall('tit')
 title_props = {}
-for child in title_el:
-  if (child.tag == 'j'):
-    title_props['j'] = child.text
-  if (child.tag == 'e'):
-    attrs = child.attrib
-    if (attrs.get('type') == 'trans' and attrs.get('ver') == '2'):
-      title_props['e'] = child.text
+for titles in title_els:
+  for child in titles:
+    if (child.tag == 'j'):
+      title_props['j'] = child.text
+    if (child.tag == 'e'):
+      attrs = child.attrib
+      if (attrs.get('type') == 'trans' and attrs.get('ver') == '2'):
+        title_props['e'] = child.text
 
-print(title_props)
+# get paragraph values
+paragraph_els = root.findall('par')
+par_props = {}
+for paragraph in paragraph_els:
+  par_key = 'par-{}'.format(paragraph.attrib['id'])
+  par_props[par_key] = {}
+  for sentence in paragraph:
+    sentence_key = 'sen-{}'.format(sentence.attrib['id'])
+    par_props[par_key][sentence_key] = {}
+    for child in sentence:
+      if (child.tag == 'j'):
+        par_props[par_key][sentence_key]['j'] = child.text
+      if (child.tag == 'e'):
+        attrs = child.attrib
+        if (attrs.get('type') == 'trans' and attrs.get('ver') == '2'):
+          par_props[par_key][sentence_key]['e'] = child.text
+
+print(json.dumps(par_props, ensure_ascii=False).encode('utf8').decode())
