fdc707eb26aa5990134b69ffe8143c1d06dbc842
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2020-08-26 16:40:13 -0700

    Ensure JSON stored as dict

--- data_parsing/txt_corpus_parser.py
+++ data_parsing/txt_corpus_parser.py
@@ -3,7 +3,7 @@ import os, json
 current_dir = os.path.dirname(__file__)
 data_path = os.path.join(current_dir, './../datasets/jesc-corpus.txt')
 
-sentence_pairs = []
+sentence_pairs = {"translations": []}
 with open(data_path, 'r', encoding='utf-8') as f:
     lines = f.read().split('\n')
 for line in lines:
@@ -13,7 +13,13 @@ for line in lines:
       english_text, japanese_text = split
       pair['e'] = english_text
       pair['j'] = japanese_text
-      sentence_pairs.append(pair)
+      sentence_pairs["translations"].append(pair)
 
 with open('subtitle_corpus.json', 'w', encoding='utf-8') as f:
-    json.dump(sentence_pairs, f, ensure_ascii=False, separators=(',', ":"))
+    json.dump(sentence_pairs, f, ensure_ascii=False)
+
+with open('subtitle_corpus.json', 'r', encoding='utf-8') as f:
+    try:
+      json_object = json.load(f)
+    except ValueError as e:
+      print('invalid JSON: ', e)
